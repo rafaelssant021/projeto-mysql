4c99b9cb7fc83448d104857f55d087004a92880b
rafaelssant021 <rafaelssant021@gmail.com>
2026-01-15 12:30:45 -0300

    new comit

---
+++ projeto-mysql.sql/SQL FIle 3.sql
@@ -0,0 +1,5 @@
+select nome, cursopreferido from gafanhotos;
+
+select nome, ano from cursos;
+
+select g.nome, c.nome, c.ano from gafanhotos as g right outer join cursos as c on c.idcurso = g.cursopreferido;
